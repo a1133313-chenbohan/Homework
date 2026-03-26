c7c3902b39d7d8f86c714218506a8da460b537c3
a1133313-bohan <a1133313@mail.nuk.edu.tw>
2026-03-26 16:35:40 +0800

    Add files via upload

---
+++ A1133313＿陳柏翰＿作業1/camp.php
@@ -0,0 +1,76 @@
+<html>
+
+<head>
+
+<title>夏令營報名表!!</title>
+
+
+</head>
+
+<body>
+<br>
+<center><font size="10" color="red">!!!!夏資一定!!!!</font></center>
+<center><font size="5" color="deeppink">²⁰²⁵高大資管夏令營《資想和你浪一下》</font></center>
+
+<br>
+    <br><font size="4">
+還沒想好夏天要去哪裡玩？<br>
+答案就是跟我們浪去墾丁一起抓住夏天的尾巴吧！</font>
+<br>
+    <br>
+
+<center><img src="../photo/one.png" width="30%"><br>
+            <a href ="https://www.instagram.com/nukim_december.118/" target ="blank">想了解更多點這邊</a>
+</center>
+
+
+<table border="0" width="100%">
+
+    <td width="50%" valign="top">
+        <li><font size="4" color="green">集合地點：管院門口
+            <li>集合時間---08:30 ~ 08:50
+                <li>09:00 準時出發，請務必準時</font></li>
+    </td>
+
+    <td width="50%" valign="top">
+      <li><font size="4" color="green">記得多帶一套換洗衣物
+        <li>活動期間有課的話記得請假!!
+            <li>平平安安出門 快快樂樂回家</font></li>
+    </td>
+
+<br>
+<table border="5">
+<tr><th></th><th>DAY1</th><th>DAY2</th><th>DAY3</th></tr>
+<tr><td>早上</td><td>抵達墾丁</td><td>闖關活動</td><td>早餐吃到飽</td></tr>
+<tr><td>下午</td><td>料理大賽</td><td>沙灘戲水</td><td>賭場活動</td></tr>
+<tr><td>晚上</td><td>酒精對決</td><td>卡拉ok</td><td>商圈購物</td></tr>
+<br>
+</table>
+
+<br>
+<center>
+    <font size="6" color="deeporange">-------------- 來報名吧 --------------</font>
+</center>
+
+<br>
+你的名字:<input type="text" palceholder="Your name" name="nName"><br>
+電話:<input type="text" palceholder="Your phone number" name="nNum"><br>
+
+E-mail
+<input type="email" name="mEmail" value="@"><br>
+
+Favor:
+葷<input type="radio" name="mMeat" value="m">
+素<input type="radio" name="mVage" value="v">
+<br>
+
+<textarea name="comment" rows="10" cols="50">
+leave somethings~~
+
+</textarea><br>
+<input type="submit">&nbsp&nbsp<input type="reset">
+
+
+</body>
+
+</html>
